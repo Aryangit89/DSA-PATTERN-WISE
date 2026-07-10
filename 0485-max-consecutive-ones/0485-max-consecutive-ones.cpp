017226811a68192be318aa5708b72b0dd656eb89
ARYAN ANKIT <aryanankitktr@gmail.com>
2026-07-10 21:47:15 +0530

    Time: 3 ms (18.72%), Space: 50.1 MB (93.55%) - LeetHub

--- 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.cpp
@@ -6,14 +6,18 @@ public:
         int res=0;
         int count=0;
         for(int high=0;high<n;high++){
-             
-        if(nums[high]==1)
+        if(nums[high]==0){
             count++;
+        }
         
+        if(count>0){
+                
+                
+                low=high+1;
+                count--;
+            }
         
-        else if(nums[high]==0)
-            count=0;
-        res=max(res,count);
+        res=max(res,high-low+1);
         }
         return res;
     }
